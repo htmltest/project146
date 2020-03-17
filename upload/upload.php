18f4c67999e344c0ffed89025d72b6db3df8b606
htmltest <struev.vs@gmail.com>
2020-03-17 23:02:22 +0300

    updates
    фото гостя, ссылка на карту

--- upload/upload.php
+++ upload/upload.php
@@ -3,15 +3,27 @@
 
     if (isset($_FILES)) {
         foreach ($_FILES as $value) {
-            $extension = pathinfo($value['name'], PATHINFO_EXTENSION);
+            if (!is_array($value['name'])) {
+                $curFile = $value;
+            } else {
+                $curFile = array();
+                foreach ($value['name'] as $subvalue) {
+                    $curFile['name'] = $subvalue['avatar'];
+                }
+                foreach ($value['tmp_name'] as $subvalue) {
+                    $curFile['tmp_name'] = $subvalue['avatar'];
+                }
+            }
+
+            $extension = pathinfo($curFile['name'], PATHINFO_EXTENSION);
 
             if (!in_array(strtolower($extension), $allowed)) {
                 echo '{"status":"error", "message":"Неверный формат файла (png, jpg, gif)"}';
                 exit;
             }
 
-            if (move_uploaded_file($value['tmp_name'], 'files/'.$value['name'])) {
-                echo '{"status":"success", "path":"' . 'upload/files/'.$value['name'] . '"}';
+            if (move_uploaded_file($curFile['tmp_name'], 'files/'.$curFile['name'])) {
+                echo '{"status":"success", "path":"' . 'upload/files/'.$curFile['name'] . '"}';
                 exit;
             }
         }
